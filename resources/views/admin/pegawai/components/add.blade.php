52e9857db7d0588f563db84f5713a481ca32a0f2
EkyGalih <ekkygalih8@gmail.com>
2023-06-12 12:02:13 +0800

    lengkapi form add pegawai

--- resources/views/admin/pegawai/components/add.blade.php
+++ resources/views/admin/pegawai/components/add.blade.php
@@ -4,6 +4,11 @@
 @section('db-pegawai', 'active')
 @section('additional-css')
     <link rel="stylesheet" href="{{ asset('server/vendor/tom-select/tom-select.css') }}">
+    <style>
+        .image_upload>input {
+            display: none;
+        }
+    </style>
 @endsection
 @section('content')
     <main id="main" class="main">
@@ -30,16 +35,24 @@
                                 @csrf
                                 <div class="row">
                                     <div class="col-lg-6">
+                                        <h4 style="text-align: center; font-weight: bold; color: #726e6e">DATA PRIBADI</h4>
+                                        <hr/>
                                         <div class="row mb-3">
-                                            <label for="inputText" class="col-sm-3 col-form-label">NIK</label>
+                                            <label for="inputText" class="col-sm-3 col-form-label">NIP</label>
                                             <div class="col-sm-9">
-                                                <input type="text" name="nik" class="form-control">
+                                                <input type="text" name="nip" class="form-control">
                                             </div>
                                         </div>
                                         <div class="row mb-3">
                                             <label for="inputText" class="col-sm-3 col-form-label">Nama Pegawai</label>
                                             <div class="col-sm-9">
                                                 <input type="text" name="name" class="form-control">
+                                            </div>
+                                        </div>
+                                        <div class="row mb-3">
+                                            <label for="inputText" class="col-sm-3 col-form-label">Pendidikan</label>
+                                            <div class="col-sm-9">
+                                                <input type="text" name="pendidikan" class="form-control">
                                             </div>
                                         </div>
                                         <div class="row mb-3">
@@ -50,6 +63,94 @@
                                             </div>
                                             <div class="col-sm-5">
                                                 <input type="text" name="tempat_lahir" class="form-control">
+                                            </div>
+                                        </div>
+                                        <div class="row mb-3">
+                                            <label for="inputText" class="col-sm-3 col-form-label">Usia</label>
+                                            <div class="col-sm-9">
+                                                <input type="text" id="umur" name="umur" class="form-control"
+                                                    disabled>
+                                            </div>
+                                        </div>
+                                        <div class="row mb-3">
+                                            <label for="inputText" class="col-sm-3 col-form-label">Jenis Kelamin</label>
+                                            <div class="col-sm-9">
+                                                <select name="jenis_kelamin" class="form-control">
+                                                    <option value="">--Pilih--</option>
+                                                    <option value="pria">Pria</option>
+                                                    <option value="wanita">Wanita</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="row mb-3">
+                                            <label for="inputText" class="col-sm-3 col-form-label">Agama</label>
+                                            <div class="col-sm-9">
+                                                <select name="agama" class="form-control">
+                                                    <option value="">--Pilih--</option>
+                                                    <option value="islam">Islam</option>
+                                                    <option value="kristen">Kristen</option>
+                                                    <option value="hindu">Hindu</option>
+                                                    <option value="budha">Budha</option>
+                                                    <option value="konghucu">Konghucu</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="row mb-3">
+                                            <label for="inputText" class="col-sm-3 col-form-label">Foto</label>
+                                            <div class="col-sm-1">
+                                                <p class="image_upload">
+                                                    <label for="userImage">
+                                                        <a class="btn btn-primary btn-sm" rel="nofollow"><span
+                                                                class='bi bi-upload'></span></a>
+                                                    </label>
+                                                    <input type="file" name="foto" id="userImage" accept="image/*"
+                                                        onchange="loadFile(event)">
+                                                </p>
+                                            </div>
+                                            <div class="col-sm-8">
+                                                <img src="{{ asset('uploads/profile/male.jpg') }}" alt="Profile" id="foto"
+                                                    style="max-width: 100%; height: 120px;">
+                                            </div>
+                                        </div>
+                                        <div class="row mb-3">
+                                            <label for="inputText" class="col-sm-3 col-form-label">Rekening</label>
+                                            <div class="col-sm-4">
+                                                <input type="text" name="nama_rekening" class="form-control"
+                                                    placeholder="Nama Bank">
+                                            </div>
+                                            <div class="col-sm-5">
+                                                <input type="text" name="no_rekening" class="form-control"
+                                                    placeholder="Nomor Rekening">
+                                            </div>
+                                        </div>
+                                    </div>
+
+
+                                    <div class="col-lg-6">
+                                        <h4 style="text-align: center; font-weight: bold; color: #726e6e">DATA PEGAWAI</h4>
+                                        <hr/>
+                                        <div class="row mb-3">
+                                            <label for="inputText"
+                                                class="col-sm-3 col-form-label">Pangkat/Golongan</label>
+                                            <div class="col-sm-5">
+                                                <select name="golonganUuid" id="golongan" placeholder="Cari.."
+                                                    autocomplete="off">
+                                                    <option value="">Cari..</option>
+                                                    @foreach ($golongan as $gol)
+                                                        <option value="{{ $gol->uuid }}">{{ $gol->nama_golongan }}
+                                                        </option>
+                                                    @endforeach
+                                                </select>
+                                            </div>
+                                            <div class="col-sm-4">
+                                                <select name="pangkatUuid" id="pangkat" placeholder="Cari.."
+                                                    autocomplete="off">
+                                                    <option value="">Cari..</option>
+                                                    @foreach ($pangkat as $pang)
+                                                        <option value="{{ $pang->uuid }}">{{ $pang->nama_pangkat }}
+                                                        </option>
+                                                    @endforeach
+                                                </select>
                                             </div>
                                         </div>
                                         <div class="row mb-3">
@@ -98,38 +199,42 @@
                                             </div>
                                         </div>
                                         <div class="row mb-3">
-                                            <label for="inputText" class="col-sm-3 col-form-label">Pendidikan</label>
+                                            <label for="inputText" class="col-sm-3 col-form-label">Kenaikan
+                                                Pangkat</label>
                                             <div class="col-sm-9">
-                                                <input type="text" name="pendidikan" class="form-control">
-                                            </div>
-                                        </div>
-                                        <div class="row mb-3">
-                                            <label for="inputText" class="col-sm-3 col-form-label">Rekening</label>
-                                            <div class="col-sm-4">
-                                                <input type="text" name="nama_rekening" class="form-control"
-                                                    placeholder="Nama Bank">
-                                            </div>
-                                            <div class="col-sm-5">
-                                                <input type="text" name="no_rekening" class="form-control"
-                                                    placeholder="Nomor Rekening">
-                                            </div>
-                                        </div>
-                                        <div class="row mb-3">
-                                            <label for="inputText" class="col-sm-3 col-form-label">Usia</label>
-                                            <div class="col-sm-9">
-                                                <input type="text" id="umur" name="umur" class="form-control"
+                                                <input type="text" name="kenaikan_pangkat" class="form-control"
                                                     disabled>
                                             </div>
                                         </div>
                                         <div class="row mb-3">
+                                            <label for="inputText" class="col-sm-3 col-form-label">Pensiun</label>
+                                            <div class="col-sm-9">
+                                                <input type="text" name="pensiun" class="form-control" disabled>
+                                            </div>
+                                        </div>
+                                        <div class="row mb-3">
+                                            <label for="inputText" class="col-sm-3 col-form-label">Bidang
+                                                Penempatan</label>
+                                            <div class="col-sm-9">
+                                                <select name="bidangUuid" id="bidang" placeholder="Cari.."
+                                                    autocomplete="off">
+                                                    <option value="">Cari..</option>
+                                                    @foreach ($bidang as $bid)
+                                                        <option value="{{ $bid->uuid }}">{{ $bid->nama_bidang }}
+                                                        </option>
+                                                    @endforeach
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="row mb-3">
                                             <div class="col-sm-12">
-                                                <button class="btn btn-outline-warning btn-md" style="float: right;"
+                                                <button class="btn btn-warning btn-md" style="float: right;"
                                                     type="reset">
                                                     <i class="bi bi-arrow-clockwise"></i> Reset
                                                 </button>
-                                                <button class="btn btn-outline-success btn-md"
+                                                <button class="btn btn-success btn-md"
                                                     style="float: right; margin-right: 2px;" type="submit">
-                                                    <i class="bi bi-save"></i> Simpan
+                                                    <i class="bi bi-plus-square"></i> Simpan
                                                 </button>
                                             </div>
                                         </div>
@@ -156,12 +261,36 @@
             createOnBlur: true,
             create: true
         });
+        new TomSelect("#bidang", {
+            persist: false,
+            createOnBlur: true,
+            create: true
+        });
+        new TomSelect("#golongan", {
+            persist: false,
+            createOnBlur: true,
+            create: true
+        });
+        new TomSelect("#pangkat", {
+            persist: false,
+            createOnBlur: true,
+            create: true
+        });
+
+        // preview image
+        var loadFile = function(event) {
+            var output = document.getElementById('foto');
+            output.src = URL.createObjectURL(event.target.files[0]);
+            output.onload = function() {
+                URL.revokeObjectURL(output.src) // free memory
+            }
+        };
 
         function monthDiff(dateFrom, dateTo) {
             if (dateFrom.getMonth() == dateTo.getMonth()) {
                 result = 0;
             } else if (dateFrom.getMonth() < dateTo.getMonth()) {
-                var count = (dateTo.getMonth()+1) - (dateFrom.getMonth()+1);
+                var count = (dateTo.getMonth() + 1) - (dateFrom.getMonth() + 1);
                 var math1 = 12 - count;
                 var result = 12 - math1
             } else {
