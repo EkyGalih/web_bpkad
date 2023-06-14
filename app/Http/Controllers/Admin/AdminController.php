76076b355f7b7cde8da157a3b8d691addf4685bd
EkyGalih <ekkygalih8@gmail.com>
2023-06-14 16:45:52 +0800

    menampilkan deleted null pada post

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -16,7 +16,7 @@ class AdminController extends Controller
         $post = Posts::where('created_at', 'LIKE', date('Y-m') . '%')->count();
         $laporan = Laporan::where('created_at', 'LIKE', date('Y-m') . '%')->count();
         $permohonan = Permohonan::where('created_at', 'LIKE', date('Y-m') . '%')->count();
-        $posts = Posts::orderBy('created_at', 'DESC')->limit(5)->get();
+        $posts = Posts::where('deleted_at', '=', NULL)->orderBy('created_at', 'DESC')->limit(5)->get();
         $lap = Laporan::where('jawaban', '!=', NULL)->where('created_at', 'LIKE' . date('Y-m') . '%')->limit(5)->get();
         $recents = DB::table('recent_activity')
             ->where('recent_activity.created_at', 'LIKE', date('Y-m-d') . '%')
