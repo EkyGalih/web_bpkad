6b40579fc877cc0f10233de27fe12e6daa4eb806
EkyGalih <ekkygalih8@gmail.com>
2023-06-15 08:53:23 +0800

    hilangkan tulisan home di beranda

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <title>@yield('title') BPKAD (Badan Pengelolaan Keuangan dan Aset Daerah</title>
+    <title>@yield('title') BPKAD Prov. NTB</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
     @include('layouts.client.css')
