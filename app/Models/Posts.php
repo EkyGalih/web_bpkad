99eba5059bac09b801e96a81e2a93886138de10a
Eky Galih <ekkygalih8@gmail.com>
2023-07-17 09:45:04 +0800

    fix time terbaru

--- app/Models/Posts.php
+++ app/Models/Posts.php
@@ -12,7 +12,7 @@ class Posts extends Model
 
     public $incrementing = false;
     protected $table = 'posts';
-    protected $guarded = ['created_at'];
+    protected $guarded = ['updated_at'];
 
     // public static function boot()
     // {
