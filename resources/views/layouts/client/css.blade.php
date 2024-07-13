2123e29f6f236b51ef2415c3b7dd6541d54cde38
EkyGalih <ekkygalih8@gmail.com>
2024-07-13 10:47:47 +0800

    UDPATE::ICON

--- resources/views/layouts/client/css.blade.php
+++ resources/views/layouts/client/css.blade.php
@@ -1,6 +1,6 @@
-<link href="{{ asset('client/assets/img/favicon.png') }}" rel="shortcut icon">
-<link href="{{ asset('client/assets/img/favicon.png') }}" rel="icon">
-<link href="{{ asset('client/assets/img/favicon.png') }}" rel="apple-touch-icon">
+<link href="{{ asset('client/assets/img/favicon.png') }}" type="image/png" rel="shortcut icon">
+<link href="{{ asset('client/assets/img/favicon.png') }}" type="image/png" rel="icon">
+<link href="{{ asset('client/assets/img/favicon.png') }}" type="image/png" rel="apple-touch-icon">
 <link
     href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
     rel="stylesheet">
