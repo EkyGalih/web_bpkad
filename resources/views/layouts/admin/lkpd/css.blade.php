5bff633e974b0e109c3224a1ad015b848c9cf531
EkyGalih <ekkygalih8@gmail.com>
2024-12-23 23:24:40 +0800

    ADD::beranda lkpd

---
+++ resources/views/layouts/admin/lkpd/css.blade.php
@@ -0,0 +1,6 @@
+<link rel="canonical" href="https://bpkad.ntbprov.go.id/admin/lkpd" />
+<link rel="shortcut icon" href="{{ asset('client/assets/img/favicon.png') }}" />
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
+<link href="{{ asset('assets/plugins/global/lkpd/plugins.bundle.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('assets/css/lkpd/style.bundle.css') }}" rel="stylesheet" type="text/css" />
+@yield('styles')
---
+++ resources/views/layouts/admin/lkpd/js.blade.php
@@ -0,0 +1,8 @@
+<script>
+    var hostUrl = "assets/";
+</script>
+<script src="{{ asset('assets/plugins/global/lkpd/plugins.bundle.js') }}"></script>
+<script src="{{ asset('assets/js/lkpd/scripts.bundle.js') }}"></script>>
+<script src="{{ asset('client/assets/js/sweet-alert.min.js') }}"></script>
+@include('layouts.sweet-alert-notification')
+@yield('scripts')
